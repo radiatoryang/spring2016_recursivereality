80a54eb01f5b1e163afe150183e62933487d555a
Robert Yang <yang.robert.w@gmail.com>
2016-02-05 21:03:32 -0500

    week02: modded first person controller to use VR camera forward instead of body's forward

--- week02/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ week02/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -97,7 +97,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
             float speed;
             GetInput(out speed);
             // always move along the camera forward as it is the direction that it being aimed at
-            Vector3 desiredMove = transform.forward*m_Input.y + transform.right*m_Input.x;
+		Vector3 desiredMove = Camera.main.transform.forward*m_Input.y + Camera.main.transform.right*m_Input.x;
 
             // get a normal for the surface that is being touched to move along it
             RaycastHit hitInfo;

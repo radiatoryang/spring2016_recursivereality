41ffdce81795957ea9894c3de0d2d71504f1a468
Robert Yang <yang.robert.w@gmail.com>
2016-04-03 21:26:10 -0400

    added week09: Photosphere demo

---
+++ week09/Assets/scripts/ButtonDemo.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonDemo : MonoBehaviour {
+
+	public Transform photoSphere; // assign in Inspector
+
+	// this function will be called by a UI button
+	// all UI functions must be "public void"
+	public void Rotate( float degrees ) {
+		photoSphere.Rotate(0f, degrees, 0f);
+	}
+		
+}
